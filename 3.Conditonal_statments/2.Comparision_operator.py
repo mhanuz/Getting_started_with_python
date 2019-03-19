7a8f52b74c5b9fda6571fd46385c37e852087a7d
mhanuz <46758023+mhanuz@users.noreply.github.com>
2019-03-20 03:03:41 +0600

    Add files via upload

---
+++ 3.Conditonal_statments/2.Comparision_operator.py
@@ -0,0 +1,17 @@
+x=10
+if x<20: # less than operator
+    print(x,' is less than 20')
+
+if x>5: # greater than operator
+    print(x,'is greater than 5')
+
+if x<=20: # less than or equal
+    print(x,'is less than or equal 20')
+
+if x>=5: # greater than or equal
+    print(x,'is greater than or equal 5')
+if x==10: # equal
+    print(x,'is equal 10')
+if x!=5:# not equal
+    print(x,'is not equal 5')
+    
---
+++ 3.Conditonal_statments/4.Multi_way_condition.py
@@ -0,0 +1,8 @@
+x=10
+if x<20: # if any condition is true than skip the all below multi way conditions
+    print('smaller')
+elif x>10:
+    print('greate')
+else:       # it is not compulsory  that in multi way condition it should use else we can avoid it
+    print('undifined')
+print('finish')
