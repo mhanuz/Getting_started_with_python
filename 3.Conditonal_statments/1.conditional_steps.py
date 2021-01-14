4e3841758705620b5e15584a78c19b605302392e
mhanuz <46758023+mhanuz@users.noreply.github.com>
2021-01-14 18:49:01 +0600

    Delete 3.Conditonal_statments directory

--- 3.Conditonal_statments/1.conditional_steps.py
+++
@@ -1,6 +0,0 @@
-x=10
-if x>5: # if condition is true than execute the block
-    print('Bigger') # statement
-if x<5:
-    print('Smaller') # print not execute because condition is false
-print('Finish')
--- 3.Conditonal_statments/3.Nested_dicision.py
+++
@@ -1,4 +0,0 @@
-x=10
-if x>1:
-    if x<100:
-        print(x,'number in between 1 and 100')
