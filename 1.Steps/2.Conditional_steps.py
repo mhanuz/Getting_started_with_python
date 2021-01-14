9c9df722eb5996f954fb7de7a3dcbda83008a67a
mhanuz <46758023+mhanuz@users.noreply.github.com>
2021-01-14 18:48:25 +0600

    Delete 1.Steps directory

--- 1.Steps/2.Conditional_steps.py
+++
@@ -1,7 +0,0 @@
-x=input('Enter a int number:')
-x=int(x)
-if x<10:
-    print('smaller')
-if x>20:
-    print('bigger')
-print('finish')
--- 1.Steps/3.Repeated_steps.py
+++
@@ -1,6 +0,0 @@
-n=input('Enter a number : ')
-n=int(n)
-while n>0:
-    print(n)
-    n-=1
-print('Blastoff!!!!')
